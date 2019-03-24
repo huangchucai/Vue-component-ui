a596d1ed1c970fac6ead58c07e47801fad6ed4c5
huangchucai <425205839@qq.com>
2019-03-24 20:41:18 +0800

    feat: 修改npm的配置

---
+++ index.js
@@ -0,0 +1,5 @@
+import Button from './src/component/button';
+import ButtonGroup from './src/component/button-group';
+import Icon from './src/component/icon';
+
+export { Button, ButtonGroup, Icon };
