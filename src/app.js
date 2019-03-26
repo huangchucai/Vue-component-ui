4dd74b5a7a2cf58002e31d80bb30e1e674a18ae0
huangchucai <425205839@qq.com>
2019-03-26 12:48:32 +0800

    feat: 完成input的基本样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import Button from './component/button';
 import Icon from './component/icon';
 import ButtonGroup from './component/button-group';
+import Input from './component/input'
+
 
 Vue.component('g-button', Button);
 Vue.component('icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
+Vue.component('g-input', Input);
 new Vue({
     el: '#app',
     data() {
